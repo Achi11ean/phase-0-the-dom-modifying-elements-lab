9be851db51191e8621a3811152961631976596f4
Jonathen Whitford <paredes.jonathen@yahoo.com>
2024-07-25 19:53:24 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+console.log('This is running');
+
+const main = document.getElementById('main');
+
+console.log('main is', main);
+
+main.remove();
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory';
+
+console.log('newHeader is ', newHeader);
+
+newHeader.innerHTML = 'Jon is the champion';
+
+document.body.append(newHeader);
